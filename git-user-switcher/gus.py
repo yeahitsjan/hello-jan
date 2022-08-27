3790c458a0c7e78e4ef19ff16f4185025aa19f66
yeahitsjan <jan.kowalewicz@web.de>
2022-08-28 01:30:10 +0200

    Implement git user switch automation

---
+++ git-user-switcher/gus.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import argparse
+import os
+
+parser = argparse.ArgumentParser("Git User Switcher")
+parser.add_argument("--user", help="Switch to $user", type=str, required=True)
+args = parser.parse_args()
+if (args.user == "tea"):
+    os.system("git config --global user.name #")
+    os.system("git config --global user.email ###")
+elif (args.user == "github"):
+    os.system("git config --global user.name #")
+    os.system("git config --global user.email ###")
+print("Git user is now:")
+os.system("git config --global user.name")
+os.system("git config --global user.email")
